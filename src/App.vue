d1ddeb441083ab4414ebb9faab7b73a1b63abeeb
MiyamotoRin <s2010293@s.tsukuba.ac.jp>
2022-07-25 16:49:00 +0900

    ロゴの指定を変えた

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo_bubble2.png">
+    <img alt="Vue logo" src="./assets/logo_bubble1.png">
     <HelloWorld msg="Hello World, enPiT!"/>
   </div>
 </template>
