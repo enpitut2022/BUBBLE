5dccbe39b84c68ef6eb01d1ebc2db83bb34b2e39
MiyamotoRin <s2010293@s.tsukuba.ac.jp>
2022-07-26 10:10:27 +0900

    サンプルの情報を表示できるようにした

---
+++ src/components/WordCloud.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{/* eslint-disable */}}
+    <ul>
+      <li v-for="data in datas">{{data.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  // const datas = {'サル痘': 723, '感染': 43224, '東京都': 111}
+  // const datas = [{'name': 'サル痘', 'num': 723}]
+  export default{
+    name: 'WordCloud',
+    data(){
+      return{
+        datas:[{'name': 'サル痘', 'num': 723}, {'name': '感染', 'num': 432},
+        {'name': '東京都', 'num':111}]
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
