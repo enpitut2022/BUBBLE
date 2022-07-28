baf5583330ed02b4b65190e72d608a2ea49334c3
Sato-Aimy <ib.ami0910@gmail.com>
2022-07-28 15:02:43 +0900

    backendの処理を変更しました。 local_host/api/wcd

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from models import get_all, init_db, insert
+from makecloud import api_bp
+
+app = Flask(__name__, static_folder='../dist/img', template_folder='../dist')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myspa.db'
+app.register_blueprint(api_bp)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        init_db(app)
+        if not get_all():
+            insert('foo', 'This is foo.')
+            insert('bar', 'This is bar.')
+    app.run()
